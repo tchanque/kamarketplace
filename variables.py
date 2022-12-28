daf6caa33b4c07748ef271a3576a8c84f03cbb1d
Thomas AUDEVIE <thomasaudevie@adminkarrossmbp.home>
2022-12-28 21:24:47 +0100

    package the functiones which extract the ids mapping

---
+++ variables.py
@@ -0,0 +1,5 @@
+DATA_PATH = "./data/"
+IDS_MAPPING_FILENAME = "Ids"
+IDS_MAPPING_PATH = DATA_PATH + IDS_MAPPING_FILENAME
+
+DECOMPILED_SOURCE_CODE_PATH = "./sources/scripts/com/ankamagames/dofus/network/messages"
